1f675ddc9984b1537bdc904fe98b85268bd366b9
Natii <Natii@DESKTOP-7K17A8A>
2020-03-03 18:18:30 +0100

    Main Modificado

--- src/main/java/org/iesalandalus/programacion/tutorias/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/tutorias/MainApp.java
@@ -1,9 +1,20 @@
 package org.iesalandalus.programacion.tutorias;
 
+import org.iesalandalus.programacion.tutorias.mvc.controlador.Controlador;
+import org.iesalandalus.programacion.tutorias.mvc.controlador.IControlador;
+import org.iesalandalus.programacion.tutorias.mvc.modelo.FactoriaFuenteDatos;
+import org.iesalandalus.programacion.tutorias.mvc.modelo.IModelo;
+import org.iesalandalus.programacion.tutorias.mvc.modelo.Modelo;
+import org.iesalandalus.programacion.tutorias.mvc.vista.FactoriaVista;
+import org.iesalandalus.programacion.tutorias.mvc.vista.IVista;
+
 public class MainApp {
 
 	public static void main(String[] args) {
-		System.out.println("Programa para la gestión de tutorías del IES Al-Ándalus");
+		Modelo modelo = new Modelo(FactoriaFuenteDatos.FICHEROS.crear());
+		IVista vista = FactoriaVista.TEXTO.crear();
+		IControlador controlador = new Controlador(modelo, vista);
+		controlador.comenzar();
 	}
 
 }
